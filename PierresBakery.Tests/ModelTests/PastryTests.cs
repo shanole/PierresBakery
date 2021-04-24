870e8de2055c26c034c429aaa576f27841f00d06
shanole <73411129+shanole@users.noreply.github.com>
2021-04-23 18:44:25 -0700

    pass test for Pastry constructor

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -6,6 +6,11 @@ namespace PierresBakery.Tests
   [TestClass]
   public class PastryTests
   {
-    
+    [TestMethod]
+   public void PastryConstructor_CreatesNewInstanceOfPastry_Pastry()
+   {
+     Pastry testPastry = new Pastry();
+     Assert.AreEqual(typeof(Pastry),testPastry.GetType());
+   } 
   }
 }
