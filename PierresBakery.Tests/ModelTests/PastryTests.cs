e14415414f3bbd63adc86beef9ab0c37a1ac8f85
shanole <73411129+shanole@users.noreply.github.com>
2021-04-23 18:47:20 -0700

    pass test for getting PastryOrders property

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -9,8 +9,15 @@ namespace PierresBakery.Tests
     [TestMethod]
    public void PastryConstructor_CreatesNewInstanceOfPastry_Pastry()
    {
-     Pastry testPastry = new Pastry();
+     Pastry testPastry = new Pastry(1);
      Assert.AreEqual(typeof(Pastry),testPastry.GetType());
-   } 
+   }
+   public void GetPastryOrders_ReturnsNumberofOrders_Int()
+   {
+     int orders = 2;
+     Pastry testPastry = new Pastry(orders);
+     int result = testPastry.PastryOrders;
+     Assert.AreEqual(orders,result);
+   }
   }
 }
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -2,6 +2,10 @@ namespace PierresBakery.Models
 {
   public class Pastry
   {
-    
+    public int PastryOrders {get; set;}
+    public Pastry(int orders)
+    {
+      PastryOrders = orders;
+    }
   }
 }
